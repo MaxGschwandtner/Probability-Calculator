45e87f9c54e8c9b89708af9d68dcd2e7063324ca
MaxGschwandtner <30653893+MaxGschwandtner@users.noreply.github.com>
2017-08-28 09:21:04 +0200

    Added formulas with MathML

--- prob.js
+++ prob.js
@@ -9,7 +9,6 @@ function fak(n){
 function calculate(){
   var k = document.querySelector('input[name="K"]').value;
   var n = document.querySelector('input[name="N"]').value;
-  // var z = document.querySelector('input[name="Z"]').value;
   var resultdiv = document.getElementById("result");
 
   var zurueck = document.getElementById("zurück").value;
@@ -17,23 +16,27 @@ function calculate(){
 
   if(zurueck == "mZ" && reihenfolge == "Rw"){
     let result = 1/Math.pow(n, k)
-    resultdiv.innerHTML = "<h3>" + result + "</h3>";
+    resultdiv.innerHTML = "<h3>" + result.toFixed(12) + "</h3>" + "<br>" + "Formel: P = <math xmlns='http://www.w3.org/1998/Math/MathML'>" +
+    "<mfrac> 1 <msup><mn> n </mn><mn> k </mn></msup> </mfrac>";
     return false;
   }
   else if(zurueck == "mZ" && reihenfolge == "Rn"){
-    var v = parseInt(n) + parseInt(k) - 1;
-    let result = 1/(fak(v)/(fak(k)*fak(n - 1)));
-    resultdiv.innerHTML = "<h3>" + result + "</h3>";
+    let v = parseInt(n) + parseInt(k) - 1;
+    let result = (fak(k)*fak(n - 1))/fak(v);
+    resultdiv.innerHTML = "<h3>" + result.toFixed(12) + "</h3>" + "<br>" + "Formel: P = <math xmlns='http://www.w3.org/1998/Math/MathML'>" +
+    "<mfrac> <mn> k! * (n - 1)! </mn> <mn> (n + k - 1)! </mn> </mfrac>";
     return false;
   }
   else if(zurueck == "oZ" && reihenfolge == "Rw"){
-    let result = 1/(fak(n)/fak(n - k));
-    resultdiv.innerHTML = "<h3>" + result + "</h3>";
+    let result = fak(n - k)/fak(n);
+    resultdiv.innerHTML = "<h3>" + result.toFixed(12) + "</h3>" + "<br>" + " Formel: P = <math xmlns='http://www.w3.org/1998/Math/MathML'>" +
+    "<mfrac> <mn> (n - k)! </mn> <mn> n! </mn> </mfrac>";
     return false;
   }
   else if(zurueck == "oZ" && reihenfolge == "Rn"){
-    let result = 1/(fak(n)/(fak(n - k)*fak(k)));
-    resultdiv.innerHTML = "<h3>" + result + "</h3>";
+    let result = fak(n - k)*fak(k)/fak(n);
+    resultdiv.innerHTML = "<h3>" + result.toFixed(12) + "</h3>" + "<br>" + "Formel: P = <math xmlns='http://www.w3.org/1998/Math/MathML'>" +
+    "<mfrac> <mn> (n - k)! * k! </mn> <mn> n! </mn> </mfrac>";
     return false;
   }
 }
